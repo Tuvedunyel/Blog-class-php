a4e75f0ad64180952da5814bb22e30acab4eb59b
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-10-06 16:04:12 +0200

    default template

---
+++ pages/templates/default.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+
+    <title>Blog</title>
+</head>
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="#" class="navbar-brand">Project name</a>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <?= $content; ?>
+    </div>
+</body>
+</html>
