20be86ea22371fd0410562121efff344db82dd69
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-10-07 09:43:08 +0200

    getter stter

--- app/Article.php
+++ app/Article.php
@@ -1,5 +1,13 @@
 <?php 
 
 class Article {
-    
+    public function getUrl() {
+        return 'index.php?p=article&id=' . $this->id;
+    }
+
+    public function getExtrait() {
+        $html = '<p>' . substr($this->contenu, 0, 400) . '...</p>';
+        $html .= '<p><a href="' . $this->getUrl() . '">Voir la suite</a></p>';
+        return $html;
+    }
 }
--- pages/home.php
+++ pages/home.php
@@ -2,12 +2,11 @@
 <?php
     foreach($db->query('SELECT * FROM articles', 'Article') as $post) : 
 ?>
-        <?php var_dump($post); ?>
     <h2>
-        <a href="<?php $post->url; ?>"><?= $post->titre; ?></a>
+        <a href="<?= $post->getUrl(); ?>"><?= $post->titre; ?></a>
     </h2>
 
-    <p><?php $post->extrait; ?></p>
+    <p><?= $post->getExtrait(); ?></p>
 
 <?php endforeach; ?>
 </ul>
