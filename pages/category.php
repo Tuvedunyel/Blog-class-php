cbed1b1d333e58d16d5a4568473ba174c400132e
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-10-07 15:38:14 +0200

    single.php

--- pages/category.php
+++ pages/category.php
@@ -1,6 +1,35 @@
 <?php 
 
 $category = Categories::find($_GET['id']);
+if ($category === false) {
+    App::notFound();
+}
 $articles = Article::lastByCategory($_GET['id']);
 $categories = Categories::all();
 ?>
+
+
+<h1><?= $category->titre; ?></h1>
+
+<div class="row">
+    <div class="col-sm-8">
+        <?php foreach ($articles as $post) : ?>
+            <h2>
+            <a href="<?= $post->url; ?>"><?= $post->titre; ?></a>,
+            </h2>
+            <p><em><?= $post->category; ?></em></p>
+            <p><?= $post->extrait; ?></p>
+        <?php endforeach; ?>
+    </div>
+    <div class="col-sm-4">
+        <ul>
+            <?php foreach (Categories::all() as $category) : ?>
+                <li>
+                    <a href="<?= $category->url; ?>"><?= $category->titre ?></a>
+                </li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+</div>
+
+
--- pages/single.php
+++ pages/single.php
@@ -1,8 +1,14 @@
 <?php 
 
-$post = App::getDatabase()->prepare('SELECT * FROM articles WHERE id = ?', [$_GET['id']], 'Article', true);
+$post = Article::find($_GET['id']);
+if ($post === false) {
+    App::notFound();
+}
+$categorie = Categories::find($post->category_id);
 ?>
 
 <h1><?= $post->titre; ?></h1>
+
+<p><em><?= $categorie->titre; ?></em></p>
 
 <p> <?= $post->contenu; ?></p>
