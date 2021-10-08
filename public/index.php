620bfad71ebaf493c93b986c99018b2cf754881a
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-10-08 09:59:30 +0200

    singleton start

--- public/index.php
+++ public/index.php
@@ -3,24 +3,7 @@
     require '../app/Autoloader.php';
     Autoloader::register(); 
 
-    if(isset($_GET['p'])) {
-        $p = $_GET['p'];
-    } else {
-        $p = 'home';
-    }
-
-    ob_start();
-
-    if ($p === 'home') {
-        require './../pages/home.php';
-    } elseif ($p === 'article') {
-        require './../pages/single.php';
-    } elseif ($p === 'categories') {
-        require ('./../pages/category.php');
-    }
-
-    $content= ob_get_clean();
-
-    require '../pages/templates/default.php';
+    $config = Config::getInstance();
+    var_dump($config);
 
     
